9f54df4c2afd4156cb334ad0b3c2a2b5828735d8
Gideon Oba <deyonoba@gmail.com>
2023-11-28 22:33:11 +0000

    modified: 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-for number in range(0, 100):
-    if number == 99:
-        print("{:d}".format(number))
-    elif 1 != number != 10:
-        print("{:02d}".format(number), end=", ")
+for i in range(0, 9):
+    for j in range(0, 10):
+        if i == 8 and j == 9:
+            print(f"{i:d}{j:d}")
+            break
+        elif i != j and i < j:
+            print(f"{i:d}{j:d}", end=", ")
