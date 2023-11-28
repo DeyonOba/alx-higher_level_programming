8c5dbf2af2d441215a5741227b75d18b438e9732
Gideon Oba <deyonoba@gmail.com>
2023-11-28 23:31:42 +0000

    Add 12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for number in range(101):
+        if number % 5 == 0:
+            print("Buzz", end=" ")
+        elif number % 3 == 0:
+            print("Fizz", end=" ")
+        else:
+            print(number, end=" ")
