eda0d908c227a3365dff395bd44c38aa2ae821c1
Gideon Oba <deyonoba@gmail.com>
2023-11-29 00:19:35 +0000

    modified: 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,7 +6,5 @@ def uppercase(string):
         ascii_no = ord(c)
         if 97 <= ascii_no <= 122:
             ascii_no -= 32
-        if 65 <= ascii_no <= 90:
-            print("{}".format(chr(ascii_no)), end='')
-        else:
-            print("{}".format(chr(ascii_no)), end='')
+        print("{}".format(chr(ascii_no)), end='')
+    print("{}".format(""))
