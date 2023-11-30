9d53e050045836d7b31eb4fa232ca5ad7b04cffb
Gideon Oba <deyonoba@gmail.com>
2023-11-30 23:03:52 +0000

    modified: 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,13 +2,16 @@
 
 import sys
 
+
 def add_argv():
+    """Add Integer command line arguments"""
     total = 0
     nargs = len(sys.argv)
 
     for i in range(1, nargs):
         total += int(sys.argv[i])
     print("{:d}".format(total))
+
 
 if __name__ == "__main__":
     add_argv()
