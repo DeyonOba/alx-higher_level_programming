233793a468264d6ac213fe7c87944d2b1cd08961
Gideon Oba <deyonoba@gmail.com>
2023-11-30 22:18:47 +0000

    modified: 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,13 +7,15 @@ import sys
 def command_line_args():
     nargv = len(sys.argv) - 1  # argv[0] contains file name
 
-    if nargv > 0:
+    if nargv > 1:
         print("{:d} arguments:".format(nargv))
-
-        for i in range(1, nargv + 1):
-            print("{:d}: {}".format(i, sys.argv[i]))
+    elif nargv == 1:
+        print("{:d} argument:".format(nargv))
     else:
         print("{:d} arguments.".format(nargv))
+
+    for i in range(1, nargv + 1):
+        print("{:d}: {}".format(i, sys.argv[i]))
 
 
 if __name__ == "__main__":
