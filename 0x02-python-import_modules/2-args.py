a2fb792783b276e2479934449ea8760631cb9b53
Gideon Oba <deyonoba@gmail.com>
2023-11-30 20:45:28 +0000

    Add 2-args.py file

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# file_name: 2-args.py
+import sys
+
+nargv = len(sys.argv) - 1  # argv[0] contains file name
+
+if nargv > 0:
+    print("{:d} arguments:".format(nargv))
+    for i in range(1, nargv + 1):
+        print("{:d}: {}".format(i, sys.argv[i]))
+else:
+    print("{:d} arguments.".format(nargv))
