2f57902a5c99d4705dfdb9eb98d06d40157612e6
Gideon Oba <deyonoba@gmail.com>
2023-12-06 22:11:53 +0000

    Add 6-print_sorted_dictionary.py

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    for key, value in sorted(a_dictionary.items()):
+        print("{}: {}".format(key, value))
+
+
+if __name__ == "__main__":
+    a_dictionary = {
+            'language': "C", 'Number': 89,
+            'track': "Low level", 'ids': [1, 2, 3]
+            }
+    print_sorted_dictionary(a_dictionary)
