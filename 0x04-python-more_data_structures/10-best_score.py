e66526dc84f7054d7765921a0bfa874234ba7430
Gideon Oba <deyonoba@gmail.com>
2023-12-06 23:29:32 +0000

    modified: 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is not None:
+        if len(a_dictionary) == 0:
+            return None
+
         name = ""
         score = 0
 
