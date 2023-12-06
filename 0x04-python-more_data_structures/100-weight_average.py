9b9a76c6c15e61029d3280093816113467a1b12f
Gideon Oba <deyonoba@gmail.com>
2023-12-06 13:04:16 +0000

    Add 100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+    """Get the Weight Average of a list"""
+    total = 0
+    n = 0
+
+    for i in my_list:
+        total += i[0] * i[1]
+        n += i[1]
+    return (total / n)
+
+
+if __name__ == "__main__":
+    my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
+    result = weight_average(my_list)
+    print("Average: {:0.2f}".format(result))
