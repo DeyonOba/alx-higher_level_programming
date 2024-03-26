949c440f46b9e4dff2d01231cc79b4768df47996
Gideon Oba <deyonoba@gmail.com>
2024-03-26 23:14:25 +0000

    Add 19-copy_list.py

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def copy_list(my_list):
+    return my_list[:]
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3]
+    print(my_list)
+
+    new_list = copy_list(my_list)
+
+    print(my_list)
+    print(new_list)
+
+    print(new_list == my_list)
+    print(new_list is my_list)
