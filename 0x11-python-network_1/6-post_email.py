a02c24de33c8b811ef40ab1ade7592584f0e23cc
Gideon Oba <deyonoba@gmail.com>
2024-03-28 21:47:44 +0000

    Add 6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Script sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+import sys
+import requests
+
+
+def main():
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    param = {"email": email}
+    r = requests.get(url, params=param)
+
+    print("Your email is:", r.content.decode())
+
+
+if __name__ == "__main__":
+    main()
