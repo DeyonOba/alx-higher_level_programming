7752a9b00cc5f3dee961e57238c6843ac2620547
Gideon Oba <deyonoba@gmail.com>
2024-03-28 22:33:42 +0000

    Add 7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Sends a request to the URL and displays the body of the response
+(decoded in utf-8)
+if the http status code is equal to or greater than 400
+Error code: followed by the HTTP status code
+else print response
+
+Usage:
+    <module_name.py> <url>
+
+Return:
+    content OR
+    error code
+"""
+
+import requests
+
+
+def main():
+    url = sys.argv[1]
+
+    r = requests.get(url)
+
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+
+    else:
+        print(r.text)
+
+
+if __name__ == "__main__":
+    main()
