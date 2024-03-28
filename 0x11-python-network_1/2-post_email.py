bf3be443724af578bdd8ed9e59d44999ffb8e3e3
Gideon Oba <deyonoba@gmail.com>
2024-03-28 15:23:08 +0000

    refactor: 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 Module takes in a URL and an email,
 sends a POST request to the passed URL with the email as a parameter,
@@ -8,6 +8,7 @@ and displays the body of the response (decoded in utf-8)
 import sys
 import urllib.request
 import urllib.parse
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
