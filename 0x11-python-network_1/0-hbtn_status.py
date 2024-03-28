def19c7961938a4ad7e8af7a04b2f89ca67453ad
Gideon Oba <deyonoba@gmail.com>
2024-03-28 09:48:25 +0000

    Add 0-hbtn_status.py file

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module fetches info from url: https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    utf8_content = body.decode("utf-8")
+
+print("Body response:")
+print(f"    - type: {type(body)}")
+print(f"    - content: {body}")
+print(f"    - utf8 content: {utf8_content}")
