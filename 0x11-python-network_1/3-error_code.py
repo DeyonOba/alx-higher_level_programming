a720c75b79afc550625d33778f6a7f5487d43559
Gideon Oba <deyonoba@gmail.com>
2024-03-28 15:49:38 +0000

    refactor: 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Sends a request to the URL and displays the body of the response (decoded in utf-8)
+Sends a request to the URL and displays the body of the response
+(decoded in utf-8)
 manage urllib.error.HTTPError exceptions and print:
 Error code: followed by the HTTP status code
 
@@ -16,6 +17,7 @@ import sys
 import urllib.request
 import urllib.error
 
+
 def main():
     url = sys.argv[1]
 
@@ -25,7 +27,7 @@ def main():
             utf8_content = body.decode("utf-8")
             print(utf8_content)
     except urllib.error.HTTPError as e:
-        print("Error code:", e.code())
+        print("Error code: {}".format(e.code()))
 
 
 if __name__ == "__main__":
