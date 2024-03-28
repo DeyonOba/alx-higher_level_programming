88ff70685d7abe0a62a1a1932c6f203b07b4183d
Gideon Oba <deyonoba@gmail.com>
2024-03-28 22:39:41 +0000

    refactor: 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -24,7 +24,7 @@ def main():
     r = requests.get(url)
 
     if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
+        print("Error code:", r.status_code)
 
     else:
         print(r.text)
