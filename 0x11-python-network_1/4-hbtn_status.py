e7c0999cd534548cb5b3a58ac337fecccc7aad8f
Gideon Oba <deyonoba@gmail.com>
2024-03-28 16:45:16 +0000

    Add 4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+
+def main():
+    url = "https://alx-intranet.hbtn.io/status"
+
+    r = requests.get(url)
+    print("Body response:")
+    print("\t- type:", type(r.text))
+    print("\t- content:", r.content.decode())
+
+
+if __name__ == "__main__":
+    main()
