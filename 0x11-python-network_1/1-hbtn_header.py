f7dfdb76f8c99771cccb88df19c6b16c7f4f32a6
Gideon Oba <deyonoba@gmail.com>
2024-03-28 13:58:44 +0000

    refactor: 1-hbtn_header.py shebang and add condition for script execution

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Python script that takes in a URL, sends a request to the URL and
 displays the value of the X-Request-Id variable found in the header
 of the response.
@@ -7,6 +7,7 @@ of the response.
 import sys
 import urllib.request
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    print(response.headers.get("X-Request-Id"))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(response.headers.get("X-Request-Id"))
