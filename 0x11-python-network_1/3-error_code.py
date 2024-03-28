c564f03930cb5a94bbcf69e32a97d1e8d8433219
Gideon Oba <deyonoba@gmail.com>
2024-03-28 15:46:12 +0000

    Add 3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Sends a request to the URL and displays the body of the response (decoded in utf-8)
+manage urllib.error.HTTPError exceptions and print:
+Error code: followed by the HTTP status code
+
+Usage:
+    <module_name.py> <url>
+
+Return:
+    content OR
+    error code
+"""
+
+import sys
+import urllib.request
+import urllib.error
+
+def main():
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            utf8_content = body.decode("utf-8")
+            print(utf8_content)
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code())
+
+
+if __name__ == "__main__":
+    main()
