aa22b2614e5b3889afcee620ad3e408b688d33f5
Gideon Oba <deyonoba@gmail.com>
2024-03-28 14:10:27 +0000

    refactor: 0-hbtn_status.py file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,12 +3,16 @@
 
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-    utf8_content = body.decode("utf-8")
+def main():
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        utf8_content = body.decode("utf-8")
 
-print("Body response:")
-print(f"    - type: {type(body)}")
-print(f"    - content: {body}")
-print(f"    - utf8 content: {utf8_content}")
+    print("Body response:")
+    print(f"    - type: {type(body)}")
+    print(f"    - content: {body}")
+    print(f"    - utf8 content: {utf8_content}")
+
+if __name__ == "__main__":
+    main()
