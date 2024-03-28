5c17d768cbc7a8ea1b0317ae2c9f284d52c55a36
Gideon Oba <deyonoba@gmail.com>
2024-03-28 21:52:21 +0000

    modified: 6-post_email.py to use the post option

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -14,7 +14,7 @@ def main():
     param = {"email": email}
     r = requests.post(url, data=param)
 
-    print("Your email is:", r.content.decode())
+    print("Your email is:", r.content.decode("utf-8"))
 
 
 if __name__ == "__main__":
