257ced39d496781897775cbefc21d4bd04eaa623
Gideon Oba <deyonoba@gmail.com>
2024-03-29 14:54:31 +0000

    Add 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module takes in a letter and sends a post request to
+http://0.0.0.0:5000/search_user with the letter as parameter
+
+The letter is stored in the parameter q,
+if no arguement is give value is an empty string
+if the response body is a proper JSON return [<id>] name
+Display Not a valid JSON if the JSON is invalid
+Display No result if the JSON is empty
+"""
+import sys
+import requests
+
+
+def main():
+    url = "http://0.0.0.0:5000/search_user"
+
+    if len(sys.argv) == 2:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+
+    payload = {"q": letter}
+    try:
+        response = requests.post(url, data=payload)
+
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data["id"], data["name"]))
+        else:
+            print("No result")
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    main()
