77a3ffe320606dbb1d0914e79204792fbf90d7b0
Gideon Oba <deyonoba@gmail.com>
2024-05-08 16:34:26 +0100

    Add 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+This module writes to a text file (UTF8) and returns the number of
+character written:
+
+    - Prototype: def write_file(filename="", text=""):
+    - Write to file using the with statement
+    - File permission or file doesn't exist exceptions are ignored.
+    - Creates the file if the file does not exist.
+    - Over-writes an already existing file.
+    - No dependencies are needed.
+"""
+
+
+def write_file(filename="", text="") -> int:
+    """
+    Writes to a text file that exists.
+
+    Parameters
+    ----------
+    filename : str
+        Combination of file path, file name, and file extension.
+    text : str
+        Text to be added to text file.
+
+    Returns
+    -------
+    char_count : int
+        The number of character written to text file
+    """
+    char_count = 0
+
+    with open(filename, mode="w", encoding="utf-8") as file:
+        char_count += file.write(text)
+
+    return(char_count)
