1a8f995db35bf2789d40b4f6d543b0429abaae91
Gideon Oba <deyonoba@gmail.com>
2024-05-10 13:45:15 +0100

    remove json module importation

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,15 +2,16 @@
 
 """
 Module returns the dictionary description with simple data structure
-(list, dictionary, string, integer and boolean) for JSON serialization of an object:
+(list, dictionary, string, integer and boolean) for JSON serialization
+of an object:
 
     - Prototype: def class_to_json(obj):
     - obj is an instance of a Class
-    - All attributes of the obj Class are serializable: 
+    - All attributes of the obj Class are serializable:
         list, dictionary, string, integer and boolean
     - You are not allowed to import any module
 """
-import json
+to_json_string = __import__("3-to_json_string").to_json_string
 
 
 def class_to_json(obj):
@@ -18,5 +19,4 @@ def class_to_json(obj):
     Dictionary description of a basic data structure.
     """
     obj_attr = obj.__dict__
-    return json.dumps(obj_attr, skipkeys=True)
-
+    return to_json_string(obj_attr)
