27744d0a9558118b6639429b5be86eea8e5ed458
Gideon Oba <deyonoba@gmail.com>
2024-06-12 12:41:22 +0100

    Add 100-append_after.py file

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Write a function that inserts a line of text to a file,
+after each line containing a specific string (see example):
+
+- Prototype: def append_after(filename="", search_string="", new_string=""):
+- You must use the with statement.
+- You don’t need to manage file permission or file doesn't exist exceptions.
+- You are not allowed to import any module.
+"""
+
+
+def append_after(filename="", search_string="", new_string="") -> None:
+    with open(filename, "r+", encoding="utf-8") as file:
+        text = file.readlines()
+        new_lines = []
+        for line in text:
+            if search_string in line:
+                new_lines.extend([line, new_string])
+            else:
+                new_lines.append(line)
+        file.seek(0)
+        file.writelines(new_lines)
