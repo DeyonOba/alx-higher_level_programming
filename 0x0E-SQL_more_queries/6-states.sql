48f6eb844049789d664cb3d4e8a76be94c291e92
Gideon Oba <deyonoba@gmail.com>
2024-06-29 01:22:13 +0100

    Add 6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Script that create a database `htbn_0d_usa` and a table `states` in the database in your MYSQL server.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS `states` (id AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
