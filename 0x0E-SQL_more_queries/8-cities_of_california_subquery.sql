dcffec179375824f8c90136dbfee953ce99e6261
Gideon Oba <deyonoba@gmail.com>
2024-06-29 14:26:18 +0100

    update table name to cities

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- script that lists all the cities of California that can be found in the database hbtn_0d_usa.
 
 SELECT id, name
-    FROM states
+    FROM cities
     WHERE state_id = (
         SELECT state_id FROM states WHERE name = 'California'
     )
